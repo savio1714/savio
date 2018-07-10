1dd42edf2a90e7e28143b9998064851948c027cb
savio1713 <saviomiranda14813@gmail.com>
2018-07-10 16:50:06 +0530

    football Sample

--- football/src/application/FootballGame.java
+++ football/src/application/FootballGame.java
@@ -8,7 +8,7 @@ public class FootballGame {
 	public static void main(String[] args) {
 		Player goalkeeper = new Player();
 		Player Striker = new Player("Mark",20,"FW",80);
-		Team Teamname = new Team("Man City");
+		Team Teamname = new Team("Man City ");
 		System.out.println("Striker:"+ "Name:" + " "+Striker.getName() + " " +  ", Age:" + Striker.getAge()+ " " + ", Position:" +Striker.getPosition()+ " " +", Height:" +Striker.getHeight());
 		goalkeeper.setAge(50);
 		//System.out.println("Goalkeeper age:"+ " " +goalkeeper.getAge());
