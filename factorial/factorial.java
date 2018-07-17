7fe32ff211e3093a30f9f953496b9aa9c8cd65ae
savio1713 <saviomiranda14813@gmail.com>
2018-07-17 15:27:20 +0530

    factorial

--- factorial/factorial.java
+++ factorial/factorial.java
@@ -1,11 +1,11 @@
 package factorial;
 
 public class factorial {
-//
+
 	public static void main(String[] args) {
 		System.out.println("factorial of a number");
-		int i,n=6,fact=1;
-		for (i=1;i<n;i++)
+		int i,n=5,fact=1;
+		for (i=1;i<=n;i++)
 		{
 			fact=fact*i;
 	
