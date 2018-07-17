47c94ae28d2ccc1c484f1fc79d3f7d148d3f2752
savio1713 <saviomiranda14813@gmail.com>
2018-07-17 16:12:12 +0530

    static factorial

--- fact/factorial.java
+++ fact/factorial.java
@@ -3,15 +3,8 @@ package fact;
 public class factorial {
 	private Integer num;
 	
-	public factorial() {
-		num=0;
-	}
-	
-	
-	public factorial(Integer num) {
-		this.num=num;
-	}
-	
+
+	//
 	public void calfactorial(Integer num) {
 		int i,n=num,fact=1;
 		for (i=1;i<=n;i++)
@@ -22,13 +15,15 @@ public class factorial {
 		this.num = fact;
 		//System.out.println("factorial of a number 5 is "+fact);
 	}
-	public Integer getNum() {
-		return num;
+	
+	public static void calstaticfactorial(Integer num) {
+		int i,n=num,fact=1;
+		for (i=1;i<=n;i++)
+		{
+			fact=fact*i;
+	
+		}
+		//this.num = fact;
+		System.out.println("factorial of a number 5 is "+fact);
 	}
-
-	public void setNum(Integer num) {
-		this.num = num;
-	}
-
-	 
 }
--- fact/factorial_main.java
+++ fact/factorial_main.java
@@ -1,12 +1,14 @@
 package fact;
-
+//
 public class factorial_main {
 
 	public static void main(String[] args) {
 		int num=5;
-	factorial fact=new factorial(num);
-	fact.calfactorial(num);
-		System.out.println("factorial of a number 5 is "+fact.getNum());
+	//factorial fact=new factorial(num);
+		System.out.println("Factorial.........");
+	factorial.calstaticfactorial(num);
+		//System.out.println("factorial of a number 5 is "+fact.getNum());
 	}
 
 }
+
