ee03a6fc495ccd8270188da82ce8dfc4c7a188d7
savio1713 <saviomiranda14813@gmail.com>
2018-07-17 14:36:42 +0530

    factorial of a number

---
+++ factorial/factorial.java
@@ -0,0 +1,17 @@
+package factorial;
+
+public class factorial {
+
+	public static void main(String[] args) {
+		System.out.println("factorial of a number");
+		int i,n=6,fact=1;
+		for (i=1;i<n;i++)
+		{
+			fact=fact*i;
+	
+		}
+		System.out.println("factorial of a number 5 is "+fact);
+
+	}
+
+}
