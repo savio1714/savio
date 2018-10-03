77e38f980b8830a2249b41ff645dcc42f2cf543c
savio1714 <saviomiranda14813@gmail.com.com>
2018-10-03 15:53:35 +0530

    Generic

---
+++ FootballGame/src/list/GenericTest.java
@@ -0,0 +1,44 @@
+package list;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+
+public class GenericTest {
+		public static void main(String []args) {
+			List<String > stringlist=new ArrayList<>();
+			List<Integer> integerlist=new ArrayList<>();
+			
+			stringlist.add("Hello");
+			stringlist.add("Hi");
+			stringlist.add("Yoo");
+			integerlist.add(22);
+			integerlist.add(2);
+			integerlist.add(12);
+			
+			printList(stringlist);
+			printList(integerlist);
+			
+			
+		}
+		/*static <E extends Number> void printList(List<E> mylist) {
+			
+			Iterator<E> myItr=mylist.iterator();
+			while(myItr.hasNext()) {
+				System.out.println("..."+myItr.next());
+			}
+		}*/
+		
+
+		 static <E> void printList(List<E> mylist) {
+			// TODO Auto-generated method stub
+			 for(E item:mylist) {
+				 System.out.println("...."+item);
+			 }
+			
+		}
+
+}
